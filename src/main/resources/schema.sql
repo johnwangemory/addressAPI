0e124f096400639100d93a9c888cd3dc915ec08a
Manav Kumar <mankumar@redhat.com>
2019-04-10 21:53:43 -0400

    add files for this project.

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Address (
+	ID varchar(10) NOT NULL,
+	City varchar(10) NOT NULL ,
+	State char(2) NOT NULL
+);
+
+INSERT INTO Address (ID,City,State) VALUES ('100','Boston','MA');
+INSERT INTO Address (ID,City,State) VALUES ('200','New York','NY');
+INSERT INTO Address (ID,City,State) VALUES ('300','Atlanta','GA');
+INSERT INTO Address (ID,City,State) VALUES ('400','Houston','TX');
+INSERT INTO Address (ID,City,State) VALUES ('500','California','CA');
